e24ae5b89dba6f32f5545cc8a1619e2c1485c52d
Joe <903111082@qq.com>
2022-10-03 15:28:07 +0800

    初始项目

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,16 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <div>
+    <h1>App 根组件</h1>
   </div>
 </template>
-
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {}
   }
 }
+</script>
+<style lang="less" scoped>
+
 </style>
