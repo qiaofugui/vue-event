7f99b7cfac34fff451483fe8e2ee8083067bc9b1
Joe <903111082@qq.com>
2022-10-05 17:43:14 +0800

    axios 响应拦截器 判断401状态 被动退出

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,6 +2,8 @@
 import axios from 'axios'
 
 import store from '@/store'
+import router from '@/router'
+import { Message } from 'element-ui'
 
 // axios.create() 创建一个带配置项的自定义 axios 函数
 const request = axios.create({
@@ -18,6 +20,22 @@ request.interceptors.request.use(function (config) {
   return config
 }, function (error) {
   // 在请求发起前的代码，如果有异常，会直接进入这里
+  return Promise.reject(error)
+})
+
+// 定义响应拦截器
+request.interceptors.response.use(function (response) {
+  // 响应状态码为 2xx 时触发成功的回调，形参中 response 是 ”成功的结果“
+  return response
+}, function (error) {
+  // 响应状态码不是 2xx 时触发的失败回调，形参中 error 是 ”失败的结果“
+  if (error.response.status === 401) {
+    // 无效的 token，把 vuex 中的 token 清空，并跳转到登录页
+    store.commit('updateToken', '')
+    store.commit('updateUserInfo', {})
+    Message.error('用户身份已过期！')
+    router.push('/login')
+  }
   return Promise.reject(error)
 })
 
