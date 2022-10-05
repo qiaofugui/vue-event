ccc87501f1b89230e48f1cf8f8c78db989232b73
Joe <903111082@qq.com>
2022-10-05 17:14:55 +0800

    未登录 路由前置守卫拦截

--- src/router/index.js
+++ src/router/index.js
@@ -25,14 +25,29 @@ const router = new VueRouter({
   routes
 })
 
+// 白名单
+const whiteList = ['/login', '/reg']
+
 // 全局前置路由守卫
 router.beforeEach((to, from, next) => {
   const token = store.state.token
-  if (token && !store.state.userInfo.username) {
-    // 本地有 token 才去请求用户信息
-    store.dispatch('getUserInfoActions')
+  if (token) {
+    // 登录了
+    if (!store.state.userInfo.username) {
+      // 本地有 token 但是没有用户信息，才去请求用户信息
+      store.dispatch('getUserInfoActions')
+    }
+    // 路由放行
+    next()
+  } else {
+    // 未登录
+    if (whiteList.includes(to.path)) {
+      next()
+    } else {
+      // 强制跳转到登录页
+      next('/login')
+    }
   }
-  next()
 })
 
 export default router
