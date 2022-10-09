2c776dac63a4bacd0424f500e9c83ee4d18a92f1
Joe <18567650610@qq.com>
2022-10-09 11:38:35 +0800

    打包发布 dist 瘦身

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,22 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_NEV === 'development' ? '/' : './'
+  publicPath: process.env.NODE_NEV === 'development' ? '/' : './',
+
+  configureWebpack: {
+    externals: {
+      // 基本格式：
+      // '包名' : '在项目中引入的名字'
+      echarts: 'echarts',
+      vue: 'Vue',
+      'vue-router': 'VueRouter',
+      vuex: 'vuex',
+      axios: 'axios',
+      dayjs: 'dayjs',
+      'element-ui': 'ELEMENT',
+      'vue-quill-editor': 'VueQuillEditor',
+      'vuex-persistedstate': 'createPersistedState'
+
+    }
+  }
 })
